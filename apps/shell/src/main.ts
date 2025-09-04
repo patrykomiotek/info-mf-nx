56e0b0987173277cfa212b3a4133152f1304a160
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-09-04 13:59:20 +0200

    feat: with manifest

--- apps/shell/src/main.ts
+++ apps/shell/src/main.ts
@@ -1,1 +1,25 @@
-import('./bootstrap').catch((err) => console.error(err));
+import { setRemoteDefinitions } from '@nx/angular/mf';
+
+// validate env
+// zod -> schema validation -> parse()
+
+// try {
+//  validationSchema.parse(process.env)
+// } catch (error) {
+
+// console.log('hello! ', process.env['NX_PUBLIC_TARGET_ENV']);
+
+const manifestFile = '/federation-manifest.json';
+
+// if (process.env['NX_PUBLIC_TARGET_ENV'] === 'local') {
+//   manifestFile = '/federation-manifest.local.json';
+// } else {
+//   throw new Error('TARGET_ENV is not set');
+// }
+
+fetch(manifestFile)
+  .then((res) => res.json())
+  .then((definitions) => setRemoteDefinitions(definitions))
+  .then(() => import('./bootstrap').catch((err) => console.error(err)));
+
+// import('./bootstrap').catch((err) => console.error(err));
