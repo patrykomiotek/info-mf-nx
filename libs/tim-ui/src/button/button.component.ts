739f70466eab99d5f80b9ad6d88b8b72553a08f8
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-09-04 10:49:57 +0200

    feat: button with props

--- libs/tim-ui/src/button/button.component.ts
+++ libs/tim-ui/src/button/button.component.ts
@@ -2,12 +2,10 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'tim-button',
-  template: `
-    <button class="bg-blue-500 text-white p-2 rounded-md">
-      {{ label }}
-    </button>
-  `,
+  templateUrl: './button.component.html',
 })
 export class ButtonComponent {
-  @Input() label = '';
+  @Input() bgColor = 'bg-blue-500';
+
+  @Input() label = 'Button';
 }
