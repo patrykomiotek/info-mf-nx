0b4902c01f7d8866e65eaf8d68ab54c7256dedde
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-09-04 11:20:47 +0200

    feat: with bulishable lib

--- apps/shell/src/app/app.ts
+++ apps/shell/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonComponent } from '@info-mf-nx/tim-ui';
-// import { TimModals } from '@tim-modals';
+import { TimModals } from '@tim-modals';
 
 @Component({
-  imports: [RouterModule, ButtonComponent],
+  imports: [RouterModule, ButtonComponent, TimModals],
   selector: 'info-mf-nx-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
